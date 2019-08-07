c0ab4f85ce1c641f7ec3366e2a3ff9852e19c461
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2019-08-07 17:59:18 +0300

    update admin site for accounts app

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from .models import Profile, SocialAccount
 
@@ -7,10 +8,20 @@ class SocialAccountInline(admin.TabularInline):
     model = SocialAccount
 
 
-
 class ProfileAdmin(admin.ModelAdmin):
     inlines = [
         SocialAccountInline,
     ]
 
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline, ]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile, ProfileAdmin)
