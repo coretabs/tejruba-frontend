c498a4bf2366522bd4eaaac4e91c855c3088ae61
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2019-08-19 16:26:41 +0300

    remove backend folder

--- backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-
-urlpatterns = [
-    path('', views.UserList.as_view()),
-    path('<int:pk>/', views.UserDetail.as_view()),
-]
--- backend/experiences/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExperiencesConfig(AppConfig):
-    name = 'experiences'
