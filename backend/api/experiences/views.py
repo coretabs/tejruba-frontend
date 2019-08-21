a30fbdb7613784c43ab4b9c9540f2b04acd6f9c9
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2019-08-21 16:26:27 +0300

    create tag and experience viewsets

--- backend/api/experiences/views.py
+++ backend/api/experiences/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import Response
 
-# Create your views here.
+from .models import Experience, Tag
+from .serializers import ExperienceSerializer, TagSerializer
+
+
+class ExperiencesViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Experience.objects.all()
+        serializer = ExperiencesSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Experience.objects.all()
+        experience = get_object_or_404(queryset, pk=pk)
+        serializer = ExperienceSerializer(experience)
+        return Response(serializer.data)
+
+
+class TagViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Tag.objects.all()
+        serializer = TagsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Tag.objects.all()
+        tag = get_object_or_404(queryset, pk=pk)
+        serializer = TagSerializer(tag)
+        return Response(serializer.data)
