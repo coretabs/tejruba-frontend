b60c64fce3dc63ed7d0c641099ee15cc9ba92589
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2019-08-26 16:47:53 +0300

    del imprt statement

--- backend/api/experiences/serializers.py
+++ backend/api/experiences/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework_recursive.fields import RecursiveField
+# from rest_framework_recursive.fields import RecursiveField
 
 from . import models
 
