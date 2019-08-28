5c3648fa8fe3ade0f4f9237cbd396c69196cd500
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2019-08-28 16:16:07 +0300

    update model field name

--- backend/api/experiences/serializers.py
+++ backend/api/experiences/serializers.py
@@ -21,11 +21,9 @@ class ExperienceSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    # author = 
-    # post = serializers.
-    author_name = serializers.CharField(source="author.username", read_only=True)
+    user_name = serializers.CharField(source="user.username", read_only=True)
     post_title = serializers.CharField(source="post.title", read_only=True)
 
     class Meta:
         model = models.Comment 
-        fields = ('pk', 'author', 'author_name', 'text', 'created_date', 'post', 'post_title')
+        fields = ('pk', 'user', 'user_name', 'text', 'created_date', 'post', 'post_title')
